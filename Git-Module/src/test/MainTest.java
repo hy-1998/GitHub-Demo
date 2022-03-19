1704ca6cf0d4d8915b27e8eb764bfe1f436641ec
‘DenimBlue’ <1184712217@qq.com>
2022-03-19 11:39:10 +0800

    test测试 增加hello

--- Git-Module/src/test/MainTest.java
+++ Git-Module/src/test/MainTest.java
@@ -7,5 +7,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("11.04测试");
         System.out.println("11.08测试");
+        System.out.println("hello master");
     }
 }
